322466e205b8c9f97368b706aaa252c5b6cc451b
1kamson2 <qwerty21028@gmail.com>
2025-03-13 01:29:25 +0100

    wip

--- python/src/main.py
+++ python/src/main.py
@@ -8,11 +8,7 @@ parser = argparse.ArgumentParser(
 
 parser.add_argument("dbname")
 parser.add_argument('username')
-parser.add_argument('db_exists')
-parser.add_argument('db_should_init')
 args = parser.parse_args()
-print(args.dbname, args.username, args.db_exists, args.db_should_init)
-print("Hello world!")
 db = Database(args.dbname, args.username)
 db.table_init() 
 
